024344c72a5aada9df5954cda818fd23a52b3371
Feres Gaaloul <32621431+feresg@users.noreply.github.com>
2018-07-09 12:05:23 +0100

    Update BaseSteganography.java

--- SteganographyClasses/BaseSteganography.java
+++ SteganographyClasses/BaseSteganography.java
@@ -1,7 +1,7 @@
 public abstract class BaseSteganography{
     // Constructor
     public BaseSteganography(){};
-    public abstract File encode(File image, String str, bool isEncrypted) throws TextTooLongException, CannotDecryptException, TextKeyTooShortException, AWTException, IOException, FileNotFoundException, ImageTooSmallException, UnsuitableImageException, TooManyColorsException, TextEmptyException;
+    public abstract File encode(File image, String str, boolean isEncrypted);
     public abstract void decode(File image);
     public abstract String[] getHeader();
     public abstract String setHeader(String message);
