894d8c9fa64fa14f60933d00d0a51764d71739af
Feres Gaaloul <Feres@HAL-9000.local>
2018-07-11 05:45:08 +0100

    refactored GifSteganography.java

--- GifSteganography.java
+++ GifSteganography.java
@@ -10,17 +10,13 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.Scanner;
 import java.awt.image.BufferedImage;
-import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
 import java.awt.image.Raster;
 import java.awt.image.WritableRaster;
 import javax.imageio.ImageIO;
-import javax.imageio.ImageReader;
 import javax.imageio.ImageTypeSpecifier;
 import javax.imageio.stream.ImageOutputStream;
-import javax.imageio.stream.ImageInputStream;
 import javax.imageio.stream.FileImageOutputStream;
-import javax.imageio.metadata.IIOMetadataNode;
 import javax.imageio.metadata.IIOMetadata;
 
 public class GifSteganography extends BaseSteganography{
@@ -28,57 +24,12 @@ public class GifSteganography extends BaseSteganography{
     private IIOMetadata[] metadatas;
     private int delayMS;
 
-    // Constructors
+    // Constructor
     public GifSteganography(File input, boolean isEncrypted) throws IOException{
         this.isEncrypted = isEncrypted;
-        this.frames = getFrames(input);
-        this.metadatas = getMetadatas(input);
-        this.delayMS = getDelayMS(input);
-    }
-    // Getters
-    public static BufferedImage[] getFrames(File gif) throws IOException {
-        ImageReader reader = ImageIO.getImageReadersByFormatName("gif").next();
-        ImageInputStream input = ImageIO.createImageInputStream(gif);
-        reader.setInput(input);
-        int count = reader.getNumImages(true);
-        BufferedImage[] frames = new BufferedImage[count];
-        for (int index = 0; index < count; index++) {
-            frames[index] = reader.read(index);
-        }
-        return frames;
-    }
-    public static IIOMetadata[] getMetadatas(File gif) throws IOException{
-        ImageReader reader = ImageIO.getImageReadersBySuffix("gif").next();
-        ImageInputStream input = ImageIO.createImageInputStream(gif);
-        reader.setInput(input);
-        int count = reader.getNumImages(true);
-        IIOMetadata[] metadatas = new IIOMetadata[count];
-        for(int index = 0; index < count; index++){
-            metadatas[index] =  reader.getImageMetadata(index);
-        }
-        return metadatas;
-    }
-    public static int getDelayMS(File gif) throws IOException{
-        ImageReader gif_img = ImageIO.getImageReadersBySuffix("gif").next();
-        ImageInputStream input = ImageIO.createImageInputStream(gif);
-        gif_img.setInput(input);
-        IIOMetadata imageMetaData =  gif_img.getImageMetadata(0);
-        String metaFormatName = imageMetaData.getNativeMetadataFormatName();
-        IIOMetadataNode root = (IIOMetadataNode)imageMetaData.getAsTree(metaFormatName);
-        IIOMetadataNode graphicsControlExtensionNode = getNode(root, "GraphicControlExtension");
-        int delayTimeMS = Integer.parseInt(graphicsControlExtensionNode.getAttribute("delayTime"));
-        return delayTimeMS;
-    }
-    private static IIOMetadataNode getNode(IIOMetadataNode rootNode, String nodeName) {
-        int nNodes = rootNode.getLength();
-        for (int i = 0; i < nNodes; i++) {
-            if (rootNode.item(i).getNodeName().compareToIgnoreCase(nodeName)== 0) {
-            return((IIOMetadataNode) rootNode.item(i));
-            }
-        }
-        IIOMetadataNode node = new IIOMetadataNode(nodeName);
-        rootNode.appendChild(node);
-        return(node);
+        this.frames = Metadata.getFrames(input);
+        this.metadatas = Metadata.getMetadatas(input);
+        this.delayMS = Metadata.getDelayMS(input);
     }
     public void writeHeader(byte[] header){
         int i=0, j=0, k=0;
